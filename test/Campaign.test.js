8dcdcf652900d3fc54c443d331abd02f26b668cb
Keshav Bansal <keshavbans@gmail.com>
2018-03-11 18:48:39 +0000

    adding more tests like manager check

--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -30,8 +30,13 @@ beforeEach(async () => {
 });
 
 describe('Campaigns', () => {
-    it('deploys a factiry and a campaign', () =>{
+    it('deploys a factory and a campaign', () =>{
         assert.ok(factory.options.address);
         assert.ok(campaign.options.address);
+    });
+
+    it('marks caller as the campaign manager', async () => {
+        const manager = await campaign.methods.manager().call();
+        assert.equal(accounts[0], manager);
     });
 });
